41739e61224d7edbef3e764c20cec5201bc77059
Flagelo6482 <frankln6482@gmail.com>
2024-01-11 21:44:39 -0500

    Feta: Update

--- Frontend/projecto-react/src/Components/Header/Header.jsx
+++ Frontend/projecto-react/src/Components/Header/Header.jsx
@@ -8,41 +8,19 @@ import {Outlet, Link} from "react-router-dom";
 export const Header = () => {
   return (
     <>
-    {/* <div className="cont_header">
+    <div className="cont_header">
         <img src={img} alt="img" className='logo'/>
 
         <ul className="cont_rutas">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/games">Games</Link></li>
-          <li><Link to="/boards">Boards</Link></li>
-          <li><Link to="/players">Players</Link></li>
-          <li><Link to="/teams">Teams</Link></li>
+          <a href="#">Home</a>
+          <a href="#">Games</a>
+          <a href="#">Boards</a>
+          <a href="#">Players</a>
+          <a href="#">Teams</a>
         </ul>
 
         <a href="Login.jsx" className='cont_login'>Start</a>
-        <Outlet />
-    </div> */}
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/games">Games</Link>
-        </li>
-        <li>
-          <Link to="/boards">Boards</Link>
-        </li>
-        <li>
-          <Link to="/players">Players</Link>
-        </li>
-        <li>
-          <Link to="/teams">Teams</Link>
-        </li>
-      </ul>
-    </nav>
-    <hr />
-    <Outlet />
+    </div>
     </>
   )
 }
